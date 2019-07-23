c54cc7b53fb1c5e079fc57a245fe830cfdadf6c1
astride09 <45978637+astride09@users.noreply.github.com>
2019-07-23 22:34:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def parse_args():
 
     parser.add_argument('--epoch', type=int, default=50, help='The number of epochs to run')
     parser.add_argument('--iteration', type=int, default=10000, help='The number of training iterations')
-    parser.add_argument('--batch_size', type=int, default=32, help='The size of batch per gpu')
+    parser.add_argument('--batch_size', type=int, default=4, help='The size of batch per gpu')
     parser.add_argument('--ch', type=int, default=96, help='base channel number per layer')
 
     # SAGAN
